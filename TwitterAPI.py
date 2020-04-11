5182f5f81426ae14a1b5356a2d022f7d16ee0bb8
Brenna Doyle <bdoylee@Brennas-MacBook-Air-2.local>
2020-04-11 16:06:21 -0700

    initalized API instance

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -1,1 +1,9 @@
+import twitter
+
+# initialize api instance
+twitter_api = twitter.Api(consumer_key='SVaUMV92pJosw7f95Fdif2Eqi',
+                        consumer_secret='fTVKKnHpsA5WbCZanXFfyxbKh0QDDsjM7ZZpYv1jIhP0IzrhUE',
+                        access_token_key='2249378049-VGOJPkBMvlaAb3m7xpySNoAIITwW274TucQq72x',
+                        access_token_secret='KdedfOf5aruqkwPhCvYSPgjLgp6FKkezjpbiCbbxFMhZM')
+
 
