62089aa777a7bd0e677e59f032ee9bcbdfa17953
Brenna Doyle <bdoylee@Brennas-MacBook-Air-2.local>
2020-04-25 05:32:27 -0700

    matplots

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -68,20 +68,92 @@ def get_all_tweets(screen_name):
     return outtweets
 
 
-#setting up database
-path = os.path.dirname(os.path.abspath(__file__))
-conn = sqlite3.connect(path + '/' + "finalprojectdatabase.db")
-cur = conn.cursor()
+
+# #setting up database
+# path = os.path.dirname(os.path.abspath(__file__))
+# conn = sqlite3.connect(path + '/' + "finalprojectdatabase.db")
+# cur = conn.cursor()
 
 all_tweets = get_all_tweets(screenname)
-count = 0
-cur.execute("DROP TABLE IF EXISTS Twitter")
-cur.execute("CREATE TABLE IF NOT EXISTS Twitter(sentiment TEXT PRIMARY KEY, date TEXT, id TEXT , tweet TEXT)")
-conn.commit()
+# count = 0
+# cur.execute("DROP TABLE IF EXISTS Twitter")
+# cur.execute("CREATE TABLE IF NOT EXISTS Twitter(sentiment TEXT PRIMARY KEY, date TEXT, id TEXT , tweet TEXT)")
+# conn.commit()
+# for tweet in all_tweets:
+#     count += 1
+#     cur.execute("INSERT OR IGNORE INTO Twitter VALUES (?, ?, ?, ?)", (tweet[0], tweet[1], tweet[2],tweet[3]))
+#     conn.commit()
+#     if count % 10 == 0:
+#         print('Pausing for a bit...')
+#         time.sleep(5)
+
+
+#february, march, and april 2020 sentiment counts
+feb_pos_count = 0
+feb_neg_count = 0
+feb_neutral_count = 0
+mar_pos_count = 0
+mar_neg_count = 0
+mar_neutral_count = 0
+apr_pos_count = 0
+apr_neg_count = 0
+apr_neutral_count = 0
 for tweet in all_tweets:
-    count += 1
-    cur.execute("INSERT OR IGNORE INTO Twitter VALUES (?, ?, ?, ?)", (tweet[0], tweet[1], tweet[2],tweet[3]))
-    conn.commit()
-    if count % 10 == 0:
-        print('Pausing for a bit...')
-        time.sleep(5)
+    date_vals = tweet[1].split("-")
+    if date_vals[1] == "02":
+        sentiment = tweet[0]
+        if sentiment == "Positive":
+            feb_pos_count += 1
+        if sentiment == "Negative":
+            feb_neg_count += 1
+        if sentiment == "Neutral":
+            feb_neutral_count += 1
+    if date_vals[1] == "03":
+        sentiment = tweet[0]
+        if sentiment == "Positive":
+            mar_pos_count += 1
+        if sentiment == "Negative":
+            mar_neg_count += 1
+        if sentiment == "Neutral":
+            mar_neutral_count += 1
+    if date_vals[1] == "03":
+        sentiment = tweet[0]
+        if sentiment == "Positive":
+            apr_pos_count += 1
+        if sentiment == "Negative":
+            apr_neg_count += 1
+        if sentiment == "Neutral":
+            apr_neutral_count += 1
+
+#creating line graphs with matplot
+pos_sentiment = [feb_pos_count, mar_pos_count, apr_pos_count]
+neg_sentiment = [feb_neg_count, mar_neg_count, apr_neg_count]
+neutral_sentiment = [feb_neutral_count, mar_neutral_count, apr_neutral_count]
+
+#positive graph
+date = ["February 2020", "March 2020", "April 2020"]
+fig, ax = plt.subplots()
+ax.plot(date, pos_sentiment)
+ax.set_xlabel("Month")
+ax.set_ylabel("Number of Tweets")
+ax.set_title("Number of POSITIVE Tweets")
+fig.savefig("positive_sentiment.png")
+plt.show()
+
+#negative graph
+fig, ax = plt.subplots()
+ax.plot(date, neg_sentiment)
+ax.set_xlabel("Month")
+ax.set_ylabel("Number of Tweets")
+ax.set_title("Number of NEGATIVE Tweets")
+fig.savefig("negative_sentiment.png")
+plt.show()
+
+#neutral graph
+fig, ax = plt.subplots()
+ax.plot(date, neutral_sentiment)
+ax.set_xlabel("Month")
+ax.set_ylabel("Number of Tweets")
+ax.set_title("Number of Neutral Tweets")
+fig.savefig("neutral_sentiment.png")
+plt.show()
