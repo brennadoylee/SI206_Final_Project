11b47988d1cc1dfd22ebc4c584829aa5190b01a9
Brenna Doyle <bdoylee@Brennas-MacBook-Air-2.local>
2020-04-25 22:59:31 -0700

    added sqlite code

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -36,145 +36,169 @@ def get_all_tweets(screen_name):
     startDate = datetime.datetime(2019, 12, 1, 0, 0, 0)
     endDate =   datetime.datetime(2020, 4, 1, 0, 0, 0)
     #initializing a list to hold all the tweepy Tweets
-    alltweets = []  
+    tweets = []  
 
     # make the initial request for most recent tweets (200 is the maximum allowed count)
     
-    new_tweets = api.user_timeline(screen_name = screenname)
+    tmpTweets = api.user_timeline(screen_name = screenname)
 
     # getting tweets from the CDC
     screen_name = "CDC"
 
-    #save most recent tweets
-    alltweets.extend(new_tweets)
-
-    #save the id of the second oldest tweet
-    # oldest = alltweets[-1].id - 1
-
-    for tweet in new_tweets:
+    #grabbing tweets between december 2019 and april 2020
+    for tweet in tmpTweets:
         if tweet.created_at < endDate and tweet.created_at > startDate:
-            alltweets.append(tweet)
+            tweets.append(tweet)
+
     # keep grabbing tweets until there are no tweets left to grab
-    # while len(new_tweets) > 0:
-    while (new_tweets[-1].created_at > startDate):
-        print("getting tweets before {}".format(new_tweets[-1].created_at))
-        new_tweets = api.user_timeline(screen_name = screenname, max_id = new_tweets[-1].id - 1)
-        for tweet in new_tweets:
+    while (tmpTweets[-1].created_at > startDate):
+        print("Last Tweet @", tmpTweets[-1].created_at, " - fetching some more")
+        tmpTweets = api.user_timeline(screen_name = screenname, max_id = tmpTweets[-1].id)
+        for tweet in tmpTweets:
             if tweet.created_at < endDate and tweet.created_at > startDate:
-                alltweets.append(tweet)
-        # using the max_id param to prevent duplicates
-        # new_tweets = api.user_timeline(screen_name = screenname,count=1000,max_id=oldest)
-
-        # #saving the most recent tweets
-        # alltweets.extend(new_tweets)
-
-        # #update the id of the second oldest tweet
-        # oldest = alltweets[-1].id - 1
-
-        # print("...{} tweets downloaded so far".format(len(alltweets)))
+                tweets.append(tweet)
 
     #organizing the data and calling tweet_analysis to get the sentiment 
+    all_tweets = [[str(tweet.id_str), str(tweet.created_at.date()), str(tweet_analysis(str(tweet.text.encode("utf-8")))), str(tweet.text.encode("utf-8"))] for tweet in tweets]
+    return all_tweets
 
-    outtweets = [[str(tweet_analysis(str(tweet.text.encode("utf-8")))), str(tweet.created_at.date()), str(tweet.id_str), str(tweet.text.encode("utf-8"))] for tweet in alltweets if "#COVID" or "#coronavirus" in tweet[3]]
-    #outtweets = [[str(tweet_analysis(str(tweet.text.encode("utf-8")))), str(tweet.created_at.date()), str(tweet.id_str), str(tweet.text.encode("utf-8"))] for tweet in alltweets]
-    return outtweets
+# to reverse the dates
+def reverse(lst): 
+    new_lst = lst[::-1] 
+    return new_lst
+
+# calling get_all_tweets to get all of the data to insert into tables
+all_tweets = get_all_tweets(screenname)
 
 
+#------------------------- EXPORTING DATA TO SQLITE DATABASE ----------------------
 
-
-#setting up database
+# setting up database
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path + '/' + "finalprojectdatabase.db")
 cur = conn.cursor()
 
-#creating Twitter table
-all_tweets = get_all_tweets(screenname)
+#creating table for sentiment analysis of only covid tweets table
 count = 0
-cur.execute("DROP TABLE IF EXISTS Tweets")
-cur.execute("CREATE TABLE IF NOT EXISTS Tweets(sentiment TEXT PRIMARY KEY, date TEXT, id TEXT , tweet TEXT)")
+cur.execute("DROP TABLE IF EXISTS TweetSentiment")
+cur.execute("CREATE TABLE IF NOT EXISTS TweetSentiment(id TEXT PRIMARY KEY, date TEXT, sentiment TEXT , tweet TEXT)")
 conn.commit()
 for tweet in all_tweets:
-    count += 1
-    cur.execute("INSERT OR IGNORE INTO Tweets VALUES (?, ?, ?, ?)", (tweet[0], tweet[1], tweet[2],tweet[3]))
+    if "COVID" in tweet[3]:
+        count += 1
+        cur.execute("INSERT OR IGNORE INTO TweetSentiment VALUES (?, ?, ?, ?)", (tweet[0], tweet[1], tweet[2],tweet[3]))
+        conn.commit()
+        if count % 10 == 0:
+            print('Pausing for a bit...')
+            time.sleep(5)
+
+print("------")
+
+#creating a table counting how many time the word covid was tweeted each day
+count = 0
+cur.execute("DROP TABLE IF EXISTS TotalTweets")
+cur.execute("CREATE TABLE IF NOT EXISTS TotalTweets(date TEXT PRIMARY KEY, corona_tweet_count INTEGER)")
+conn.commit()
+corona_tweet_count = {}
+all_tweet_count = {}
+for tweet in all_tweets:
+    date = tweet[1]
+    text = tweet[3]
+    if "COVID" in text:
+        if date not in corona_tweet_count:
+            corona_tweet_count[date] = 1
+        else:
+            corona_tweet_count[date] += 1
+
+for key in corona_tweet_count:
+    cur.execute("INSERT OR IGNORE INTO TotalTweets VALUES (?, ?)", (key, corona_tweet_count[key]))
     conn.commit()
     if count % 10 == 0:
         print('Pausing for a bit...')
         time.sleep(5)
 
-#creating a dictionary of every tweet about coronavirus published by the CDC per day
-corona_tweet_count = {}
-for tweet in all_tweets:
-    if tweet[1] not in corona_tweet_count:
-        corona_tweet_count[tweet[1]] = 1
-    corona_tweet_count[tweet[1]] += 1
 
-#creating dictionaries for number of positive, negative, and neutral tweets about coronavirus published by the CDC per day
-pos_tweet_count = {}
-neg_tweet_count = {}
-neutral_tweet_count = {}
-for tweet in all_tweets:
-    date = tweet[1]
-    sentiment = tweet[0]
-    if sentiment == "Positive":
-        if tweet[1] not in pos_tweet_count:
-            pos_tweet_count[tweet[1]] = 1
-        pos_tweet_count[tweet[1]] += 1
-    if sentiment == "Negative":
-        if tweet[1] not in neg_tweet_count:
-            neg_tweet_count[tweet[1]] = 1
-        neg_tweet_count[tweet[1]] += 1
-    if sentiment == "Neutral":
-        if tweet[1] not in neutral_tweet_count:
-            neutral_tweet_count[tweet[1]] = 1
-        neutral_tweet_count[tweet[1]] += 1
+# -------------- PULLING DATA / CREATING MATPLOTLIB GRAPHS --------------------
 
-#seperating the dates and tweets into lists from the dictionaries
+# pulling the date and corona_tweet_count from the TotalTweets data
+def covid_tweet_count(cur = cur, conn = conn):
+     cur.execute('SELECT date, corona_tweet_count FROM TotalTweets')
+     data = cur.fetchall()
+     return data
+
+# calling hte function and organizing the data
+covid_tweet_count = covid_tweet_count(cur = cur, conn = conn)
 total_dates = []
-total_tweets = []
-for key in corona_tweet_count:
-    total_dates.append(key)
-    total_tweets.append(corona_tweet_count[key])
+tweet_count = []
+tweet_count.append(0)
+for tweet in covid_tweet_count:
+    total_dates.append(tweet[0])
+    tweet_count.append(tweet[1])
+total_dates.append("2019-12-01")
+    
 
-pos_dates = []
-pos_tweets = []
-for key in pos_tweet_count:
-    pos_dates.append(key)
-    pos_tweets.append(pos_tweet_count[key])
-
-neg_dates = []
-neg_tweets = []
-for key in neg_tweet_count:
-    neg_dates.append(key)
-    neg_tweets.append(neg_tweet_count[key])
-
-neutral_dates = []
-neutral_tweets = []
-for key in neutral_tweet_count:
-    neutral_dates.append(key)
-    neutral_tweets.append(neutral_tweet_count[key])
-
-# to reverse the dates on the x axis
-def reverse(lst): 
-    new_lst = lst[::-1] 
-    return new_lst
-
-
-# creating total Number of Tweets per day from the CDC containing #COVID or #coronavirus graph
-
+# creating a graph of the number of Covid related tweets per day from the TotalTweet Data
 fig = plt.figure(figsize = (10,5))
 ax = fig.add_subplot(111)
-ax.plot(reverse(total_dates), total_tweets, color = "green")
+ax.plot(reverse(total_dates), tweet_count, color = "green")
 plt.xticks(fontsize = 5, rotation = 90)
 ax.xaxis.set_major_locator(ticker.LinearLocator(10))
 ax.set_xlabel("Dates")
 ax.set_ylabel("Number of Tweets")
-ax.set_title("Number of Tweets per day from the CDC containing #COVID or #coronavirus since December 1, 2019")
+ax.set_title("Number of Tweets the CDC has Released containing the word 'COVID' since Deceber 1, 2019")
 ax.grid()
 fig.savefig("total_tweets.png")
 plt.show()
 
-# creating sentiment Analysis of tweets per day from the CDC containing #COVID or #coronavirus graph
+# pulling the dates and sentiments from the TweetSentiment table
+def tweet_sentiment(cur = cur, conn = conn):
+    cur.execute('SELECT date, sentiment FROM TweetSentiment')
+    data = cur.fetchall()
+    return data
 
+# calling the function and getting the number of same sentiment analyisis of all covid related tweets
+pos_tweet_count = {}
+neg_tweet_count = {}
+neutral_tweet_count = {}
+tweet_sentiment = tweet_sentiment(cur,conn)
+dates = []
+for tweet in tweet_sentiment:
+    date = tweet[0]
+    dates.append(date)
+    sentiment = tweet[1]
+    if sentiment == "Positive":
+        if date not in pos_tweet_count:
+            pos_tweet_count[date] = 1
+        pos_tweet_count[date] += 1
+    if sentiment == "Negative":
+        if date not in neg_tweet_count:
+            neg_tweet_count[date] = 1
+        neg_tweet_count[date] += 1
+    if sentiment == "Neutral":
+        if date not in neutral_tweet_count:
+            neutral_tweet_count[date] = 1
+        neutral_tweet_count[date] += 1
+
+#organizing the counts into lists
+pos_tweets = []
+pos_dates = []
+pos_tweets.append(0)
+for key in pos_tweet_count:
+    pos_tweets.append(pos_tweet_count[key])
+    pos_dates.append(key)
+pos_dates.append("2019-12-01")
+
+neg_tweets = []
+neg_tweets.append(0)
+for key in neg_tweet_count:
+    neg_tweets.append(neg_tweet_count[key])
+
+neutral_tweets = []
+neutral_tweets.append(0)
+for key in neutral_tweet_count:
+    neutral_tweets.append(neutral_tweet_count[key])
+
+#creating a graph of the sentiment analysis of the cvid related tweets per day from the TweetSentiment table
 fig = plt.figure(figsize = (10,5))
 ax = fig.add_subplot(111)
 ax.plot(reverse(pos_dates), pos_tweets, color = "green", label = "positive")
@@ -184,7 +208,7 @@ plt.xticks(fontsize = 5, rotation = 90)
 ax.xaxis.set_major_locator(ticker.LinearLocator(10))
 ax.set_xlabel("Dates")
 ax.set_ylabel("Number of Tweets")
-ax.set_title("Sentiment Analysis of tweets per day from the CDC containing #COVID or #coronavirus since December 1, 2019")
+ax.set_title("Sentiment Analysis of Every Tweet Containing the word 'COVID' released by the CDC since December 1, 2019")
 ax.grid()
 ax.legend()
 fig.savefig("setiment_analysis.png")
